fa31c9138b1f61eb00e0f10d15abd0eb8f8e3723
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-03-21 11:43:10 -0600

    basic country detail page setup

--- components/CountryCard.js
+++ components/CountryCard.js
@@ -1,23 +1,26 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function CountryCard({ country }) {
   const { name, flags, population, region, capital } = country
   const formattedPopulation = population.toLocaleString('en-US')
-  
+
   return (
-    <div className='shadow-md'>
-      <Image
-        src={flags.svg}
-        alt={`Flag of ${name.common}`}
-        width={900}
-        height={600}
-        layout='responsive' />
-      <div className='px-5 pt-5 pb-10 bg-white'>
-        <h2 className='font-bold text-xl mb-4'>{name.common}</h2>
-        <p className='mb-1 text-homepage'><span className='font-semibold'>Population:</span> {formattedPopulation}</p>
-        <p className='mb-1 text-homepage'><span className='font-semibold'>Region:</span> {region}</p>
-        <p className='mb-1 text-homepage'><span className='font-semibold'>Capital:</span> {capital}</p>
+    <Link href={`country/${country.cca2.toLowerCase()}`} passHref>
+      <div className='shadow-md'>
+        <Image
+          src={flags.svg}
+          alt={`Flag of ${name.common}`}
+          width={900}
+          height={600}
+          layout='responsive' />
+        <div className='px-5 pt-5 pb-10 bg-white'>
+          <h2 className='font-bold text-xl mb-4'>{name.common}</h2>
+          <p className='mb-1 text-homepage'><span className='font-semibold'>Population:</span> {formattedPopulation}</p>
+          <p className='mb-1 text-homepage'><span className='font-semibold'>Region:</span> {region}</p>
+          <p className='mb-1 text-homepage'><span className='font-semibold'>Capital:</span> {capital}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
