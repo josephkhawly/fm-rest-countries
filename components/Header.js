1e40ee6143c8541f04274c4ed4233c2c00af7cfd
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-03-26 21:53:48 -0600

    toggle icon

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import { useTheme } from 'next-themes'
 import Link from 'next/link'
+import { MoonIcon } from '@heroicons/react/outline'
+import { MoonIcon as FilledMoonIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   const { theme, setTheme } = useTheme()
@@ -10,9 +12,14 @@ export default function Header() {
           <a className="text-2xl font-extrabold">Where in the world?</a>
         </Link>
         <button
-          className=""
+          className="flex items-center"
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
         >
+          {theme === 'dark' ? (
+            <FilledMoonIcon className="mr-3 h-5 w-5" />
+          ) : (
+            <MoonIcon className="mr-3 h-5 w-5" />
+          )}{' '}
           Dark Mode
         </button>
       </div>
